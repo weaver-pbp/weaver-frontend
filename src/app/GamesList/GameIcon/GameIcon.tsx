0e9d687e414c405a701507f30e0d58fa5c2ec139
William Gooch <william.gooch.wg@gmail.com>
2020-11-20 15:32:19 +0000

    Add labels for game names to sidebar.

--- src/app/GamesList/GameIcon/GameIcon.tsx
+++ src/app/GamesList/GameIcon/GameIcon.tsx
@@ -11,17 +11,20 @@ interface GameIconProps {
 const GameIcon: React.FC<GameIconProps> = (props) => {
     return (
         <button className={styles["icon-button"]} onClick={props.select}>
-            <figure className="image is-64x64 m-2">
-                <img
-                    className={
-                        props.isSelected
-                            ? styles["is-active"]
-                            : styles["is-inactive"]
-                    }
-                    src="https://bulma.io/images/placeholders/64x64.png"
-                    alt={props.gameName}
-                />
-            </figure>
+            <span className={styles["content"]} tabIndex={-1}>
+                <figure className="image is-64x64 m-2">
+                    <img
+                        className={
+                            props.isSelected
+                                ? styles["is-active"]
+                                : styles["is-inactive"]
+                        }
+                        src="https://bulma.io/images/placeholders/64x64.png"
+                        alt={props.gameName}
+                    />
+                </figure>
+            </span>
+            <div className={styles["label"]}>{props.gameName}</div>
         </button>
     );
 };
